f89b22a07093eb2fd44f3b80945ee9bc205b13ef
ashutosh <srivastava_ashutosh@lilly.com>
2023-08-22 19:29:36 +0530

    add two more modules to test dependabot vulnerabilities for multi modules

--- firstModule/build.gradle.kts
+++ firstModule/build.gradle.kts
@@ -152,4 +152,19 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+
+    // Play In-App Review dependencies
+    implementation("com.google.android.play:review:2.0.1")
+    implementation("com.google.android.play:review-ktx:2.0.1")
+
+    //Compose
+    implementation("androidx.activity:activity-compose:1.7.2")
+    implementation("androidx.constraintlayout:constraintlayout-compose:1.0.1")
+    implementation("androidx.compose.ui:ui:1.4.3")
+    implementation("androidx.compose.material:material:1.4.3")
+    implementation("androidx.navigation:navigation-compose:2.5.3")
+    implementation("com.google.accompanist:accompanist-navigation-animation:0.30.0")
+    implementation("com.google.accompanist:accompanist-pager:0.30.0")
+    implementation("androidx.compose.ui:ui-util:1.4.3")
+    implementation("com.google.accompanist:accompanist-webview:0.30.0")
 }
